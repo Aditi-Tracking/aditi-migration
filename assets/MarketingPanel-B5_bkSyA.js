dd4a4d0e5799325cbe5e575a99b6c05132a978a3
Hemant <mis1@adititracking.com>
2026-09-18 18:56:14 +0530

    Updates

---
+++ assets/MarketingPanel-B5_bkSyA.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-BuqwL3YU.js";import{t}from"./CNSectionPanel-4zG6T04G.js";var n=e();function r(){return(0,n.jsx)(t,{sectionName:`Marketing`,title:`Marketing`,breadcrumb:`Home › Marketing`})}export{r as default};
---
+++ assets/ProductsPanel-B_I75LWP.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-BuqwL3YU.js";import{t}from"./CNSectionPanel-4zG6T04G.js";var n=e();function r(){return(0,n.jsx)(t,{sectionName:`Products`,title:`Products`,breadcrumb:`Home › Products`})}export{r as default};
