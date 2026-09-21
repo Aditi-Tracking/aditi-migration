9945ff1e3b375839bc45a554f23831556f827562
Hemant <mis1@adititracking.com>
2026-09-21 18:16:17 +0530

    Updates

---
+++ assets/AfterSalesPanel-B3KB9SlD.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-BuqwL3YU.js";import{t}from"./CNSectionPanel-Bo7xco0z.js";var n=e();function r(){return(0,n.jsx)(t,{sectionName:`After Sales`,title:`After Sales`,breadcrumb:`Home › After Sales`})}export{r as default};
